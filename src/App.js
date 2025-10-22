723b3542fadf40e207bce3006d8d18b91b2f5424
PreshStoner <preshstoner@gmail.com>
2025-10-22 06:47:47 +0100

    Fixed the index.js file so It can display on the browser page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
-import Userfront from "@userfront/react";
-
-Userfront.init("demo1234");
-
-const SignupForm = Userfront.build({
-  toolId: "nkmbbm",
-});
-const LoginForm = Userfront.build({
-  toolId: "alnkkd",
-});
-const PasswordResetForm = Userfront.build({
-  toolId: "dkbmmo",
-});
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './App.css';
 
 export default function App() {
   return (
@@ -61,56 +43,18 @@ export default function App() {
 }
 
 function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-      <SignupForm />
-    </div>
-  );
+  return <h2>Home</h2>
 }
 
 function Login() {
-  return (
-    <div>
-      <h2>Login</h2>
-      <LoginForm />
-    </div>
-  );
+  return <h2>Login</h2>
 }
 
 function PasswordReset() {
-  return (
-    <div>
-      <h2>Password Reset</h2>
-      <PasswordResetForm />
-    </div>
-  );
+  return <h2>Password Reset</h2>
 }
 
 function Dashboard() {
-  function renderFn({ location }) {
-    // If the user is not logged in, redirect to login
-    if (!Userfront.accessToken()) {
-      return (
-        <Redirect
-          to={{
-            pathname: "/login",
-            state: { from: location },
-          }}
-        />
-      );
-    }
-
-    // If the user is logged in, show the dashboard
-    const userData = JSON.stringify(Userfront.user, null, 2);
-    return (
-      <div>
-        <h2>Dashboard</h2>
-        <pre>{userData}</pre>
-        <button onClick={Userfront.logout}>Logout</button>
-      </div>
-    );
-  }
-
-  return <Route render={renderFn} />;
+  return <h2>Dashboard</h2>
 }
+
