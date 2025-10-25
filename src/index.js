6e56e793cbc8fecf337e2be5e83abffd540b470a
PreshStoner <preshstoner@gmail.com>
2025-10-25 02:33:35 +0100

    Since Userfront doesn't support React 19 yet, downgrade to React 18

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//Get the root element
-const container = document.getElementById('root');
+// //Get the root element
+// const container = document.getElementById('root');
 
 //Create a root and render the app
-const root = createRoot(container);
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
